54231c392d2873d8d15c65421c2058c70f6934f6
Sanjog Thapa <sanzog03@gmail.com>
2026-01-21 10:41:00 -0600

    type of reliability score to be float

--- akd_ext/tools/code_search/repository_search.py
+++ akd_ext/tools/code_search/repository_search.py
@@ -74,7 +74,7 @@ class RepositorySearchTool(SDECodeSearchTool):
     owner, repo = path_parts[0], path_parts[1]
     repo_name = f"{owner}/{repo}"
     repository_metadata: RepositoryMetadata = await fetch_github_metadata(repo_name, self.config.access_token)
-    reliability_score: int | float = calculate_reliability_score(repository_metadata)
+    reliability_score: float = calculate_reliability_score(repository_metadata)
     return RepositorySearchResultItem(**{
       **repository_item.model_dump(),
       "repository_metadata": repository_metadata,
