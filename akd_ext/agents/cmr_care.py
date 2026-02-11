773c22f77ad59ada7efe7ca92c272c0904a3b8e1
NISH1001 <nishanpantha@gmail.com>
2026-02-10 21:58:00 -0600

    Bugfix orchestrator followup query message

--- akd_ext/agents/cmr_care.py
+++ akd_ext/agents/cmr_care.py
@@ -618,6 +618,9 @@ class CMRCareAgent(OpenAIBaseAgent[CMRCareAgentInputSchema, CMRCareAgentOutputSc
                 if not messages:
                     messages.append(self._default_system_message())
 
+                if params and not (run_context and run_context.human_response):
+                    messages.append({"role": "user", "content": params.model_dump_json(exclude={"type"})})
+
                 run_context.messages = messages
 
                 yield RunningEvent(
